c706aa6fe5a7c5b9d1e053dd1e38310dd64f4dcf
master <spikerus93@rambler.ru>
2023-08-28 20:51:50 +0300

    выполнение задачи 20 с доп. решением-2 по семинару 3

--- HWSeminar3/hw_task20.py
+++ HWSeminar3/hw_task20.py
@@ -23,28 +23,28 @@
 # Вывод:
 # 12
 
-eng = {1:'AEIOULNSTR',
-      	2:'DG',
-      	3:'BCMP',
-      	4:'FHVWY',
-      	5:'K',
-      	8:'JZ',
-      	10:'QZ'}
-rus = {1:'АВЕИНОРСТ',
-      	2:'ДКЛМПУ',
-      	3:'БГЁЬЯ',
-      	4:'ЙЫ',
-      	5:'ЖЗХЦЧ',
-      	8:'ШЭЮ',
-      	10:'ФЩЪ'}
-N = abs(int(input('Введите 1, если играем в английской раскладке, либо 0, если в русской: ')))
-word = input('Введите слово: ').upper()
-if N == 1:
-	print('За это слово вы получаете', sum([k for i in word for k, v in eng.items() if i in v]), 'очков')
-elif N == 0:
-	print('За это слово вы получаете', sum([k for i in word for k, v in rus.items() if i in v]), 'очков')
-else:
-    print('Вы мухлюете и играете не по правилам!')
+# eng = {1:'AEIOULNSTR',
+#       	2:'DG',
+#       	3:'BCMP',
+#       	4:'FHVWY',
+#       	5:'K',
+#       	8:'JZ',
+#       	10:'QZ'}
+# rus = {1:'АВЕИНОРСТ',
+#       	2:'ДКЛМПУ',
+#       	3:'БГЁЬЯ',
+#       	4:'ЙЫ',
+#       	5:'ЖЗХЦЧ',
+#       	8:'ШЭЮ',
+#       	10:'ФЩЪ'}
+# N = abs(int(input('Введите 1, если играем в английской раскладке, либо 0, если в русской: ')))
+# word = input('Введите слово: ').upper()
+# if N == 1:
+# 	print('За это слово вы получаете', sum([k for i in word for k, v in eng.items() if i in v]), 'очков')
+# elif N == 0:
+# 	print('За это слово вы получаете', sum([k for i in word for k, v in rus.items() if i in v]), 'очков')
+# else:
+#     print('Вы мухлюете и играете не по правилам!')
     
 # Доп. решение.
     
@@ -64,29 +64,29 @@ else:
 # print(count)
 
 # Доп. решение 2.
-# eng = 'qwertyuiopasdfghjklzxcvbnm'
+eng = 'qwertyuiopasdfghjklzxcvbnm'
 
-# rus = 'йцукенгшщзхъфывапролджэячсмитьбюё'
+rus = 'йцукенгшщзхъфывапролджэячсмитьбюё'
 
-# list_English = {1:'AEIOULNSTR', 2:'DG', 3:'BCMP',
-#                 4:'FHVWY', 5:"K" , 8:'JX', 10:'QZ'}
-# list_Russian = {1:'АВЕИНОРСТ', 2:'ДКЛМПУ', 3:'БГЁЬЯ',
-#                 4:'ЙЫ', 5:'ЖЗХЦЧ', 8:'ШЭЮ', 10:'ФЩЪ'}
+list_English = {1:'AEIOULNSTR', 2:'DG', 3:'BCMP',
+                4:'FHVWY', 5:"K" , 8:'JX', 10:'QZ'}
+list_Russian = {1:'АВЕИНОРСТ', 2:'ДКЛМПУ', 3:'БГЁЬЯ',
+                4:'ЙЫ', 5:'ЖЗХЦЧ', 8:'ШЭЮ', 10:'ФЩЪ'}
 
-# k = input('Введите слово: ')
+k = input('Введите слово: ')
 
-# if k[0].lower() in eng:
-#      sum = 0
-#      for letter in k:
-#         for key, value in list_English.items():
-#             if letter.upper() in value:
-#                 sum += key
-#         print(sum)
-# else:
-#     if k[0].lower() in rus:
-#         sum = 0
-#         for letter in k:
-#             for key, value in list_Russian.items():
-#                 if letter.upper() in value:
-#                     sum += key
-#         print(sum)
+if k[0].lower() in eng:
+     sum = 0
+     for letter in k:
+        for key, value in list_English.items():
+            if letter.upper() in value:
+                sum += key
+        print(sum)
+else:
+    if k[0].lower() in rus:
+        sum = 0
+        for letter in k:
+            for key, value in list_Russian.items():
+                if letter.upper() in value:
+                    sum += key
+        print(sum)
