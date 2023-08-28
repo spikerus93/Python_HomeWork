96f068181385482a8c5c740b80f1296d985bcdb6
master <spikerus93@rambler.ru>
2023-08-28 20:35:22 +0300

    выполнение задачи 20 с доп. решением по семинару 3

--- HWSeminar3/hw_task20.py
+++ HWSeminar3/hw_task20.py
@@ -45,3 +45,48 @@ elif N == 0:
 	print('За это слово вы получаете', sum([k for i in word for k, v in rus.items() if i in v]), 'очков')
 else:
     print('Вы мухлюете и играете не по правилам!')
+    
+# Доп. решение.
+    
+# points_en = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP', 4: 'FHVWY', 5: 'K', 8: 'JX', 10: 'QZ'}
+# points_ru = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ', 4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
+# word = k.upper()  # переводим все буквы в верхний регистр
+# count = 0
+# for i in word:
+#     if i in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+#         for j in points_en:
+#             if i in points_en[j]:
+#                 count = count + j
+#     else:
+#         for j in points_en:
+#             if i in points_ru[j]:
+#                 count = count + j
+# print(count)
+
+# Доп. решение 2.
+# eng = 'qwertyuiopasdfghjklzxcvbnm'
+
+# rus = 'йцукенгшщзхъфывапролджэячсмитьбюё'
+
+# list_English = {1:'AEIOULNSTR', 2:'DG', 3:'BCMP',
+#                 4:'FHVWY', 5:"K" , 8:'JX', 10:'QZ'}
+# list_Russian = {1:'АВЕИНОРСТ', 2:'ДКЛМПУ', 3:'БГЁЬЯ',
+#                 4:'ЙЫ', 5:'ЖЗХЦЧ', 8:'ШЭЮ', 10:'ФЩЪ'}
+
+# k = input('Введите слово: ')
+
+# if k[0].lower() in eng:
+#      sum = 0
+#      for letter in k:
+#         for key, value in list_English.items():
+#             if letter.upper() in value:
+#                 sum += key
+#         print(sum)
+# else:
+#     if k[0].lower() in rus:
+#         sum = 0
+#         for letter in k:
+#             for key, value in list_Russian.items():
+#                 if letter.upper() in value:
+#                     sum += key
+#         print(sum)
